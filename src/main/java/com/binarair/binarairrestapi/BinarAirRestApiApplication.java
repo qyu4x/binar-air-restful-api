c3ba80dd2adf82245fc0024748d6af97a5a885bf
Ariq Khoiri <qq.khoiri@gmail.com>
2022-11-22 08:11:28 +0700

    Initial commit for our project

---
+++ src/main/java/com/binarair/binarairrestapi/BinarAirRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.binarair.binarairrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarAirRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinarAirRestApiApplication.class, args);
+	}
+
+}
