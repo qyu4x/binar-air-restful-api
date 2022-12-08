72006200f3dda21b4aabd714a96d4c37e01f0a53
Ariq Khoiri <qq.khoiri@gmail.com>
2022-12-08 11:44:09 +0700

    update:configuration for production

--- src/main/java/com/binarair/binarairrestapi/security/SecurityConfiguration.java
+++ src/main/java/com/binarair/binarairrestapi/security/SecurityConfiguration.java
@@ -57,6 +57,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
